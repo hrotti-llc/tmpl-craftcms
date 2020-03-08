9a4274b25baa0aaf311e5d6e8d0118f5baae73f5
Jesse Finn <contact@jessefinn.com>
2020-03-08 15:56:12 -0400

    WIP

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
 
 require_once CRAFT_VENDOR_PATH . '/autoload.php';
 
-if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH.'/.env')) {
+if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH . '/.env')) {
 
 	Dotenv\Dotenv::create(CRAFT_BASE_PATH)->load();
 
