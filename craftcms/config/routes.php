a0a773c5d5ae52b66fae7f7dbb3b442e6a2c9838
Jesse Finn <contact@jessefinn.com>
2020-03-09 18:23:50 -0400

    WIP

--- craftcms/config/routes.php
+++ craftcms/config/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-
+	'api' => 'graphql/api'
 ];
--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-define('CRAFT_BASE_PATH', realpath(dirname(__DIR__) . '/craftcms'));
+define('CRAFT_COMPOSER_PATH', realpath(dirname(__DIR__) . '/../www/composer.json'));
+
+define('CRAFT_BASE_PATH', realpath(dirname(__DIR__) . '/../www/craftcms'));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
 
 require_once CRAFT_VENDOR_PATH . '/autoload.php';
@@ -13,6 +15,6 @@ if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH . '/.env')) {
 
 define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
 
-$app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
+$app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 
 $app->run();
