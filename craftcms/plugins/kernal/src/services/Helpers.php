b75f4e0b42ac28e83512b918047f4075118e3c36
Jesse Finn <hello@jessefinn.com>
2021-08-18 19:18:55 -0400

    Removed additional linebreaks in Helpers.

--- craftcms/plugins/kernal/src/services/Helpers.php
+++ craftcms/plugins/kernal/src/services/Helpers.php
@@ -23,328 +23,318 @@ use craft\elements\db\CategoryQuery;
 
 class Helpers extends \hrotti\kernal\helpers\CommonService {
 
-	use \hrotti\kernal\helpers\SecurityHelper;
+    use \hrotti\kernal\helpers\SecurityHelper;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * globals
+     *
+     * @param int $limit 
+     * @param int $page 
+     * @param array $status 
+     * @param mixed $filters 
+     *
+     * @return \craft\elements\db\GlobalSetQuery
+     */
+    public function globals(
+        int $limit = 20, 
+        int $page = 1, 
+        array $status = ['live'], 
+        mixed $filters = null
+    ) : \craft\elements\db\GlobalSetQuery {
+
+        $query = new GlobalSetQuery(GlobalSet::class);
+
+        $this->normalizeTagsQueryFilters($query, $filters);
+        $this->normalizeSortingFilters($query, $filters);
+
+        return $query;
+
+    }
+
+    /**
+     * tags
+     *
+     * @param int $limit 
+     * @param int $page 
+     * @param array $status 
+     * @param mixed $filters 
+     *
+     * @return \craft\elements\db\TagQuery
+     */
+    public function tags(
+        $limit = 20, 
+        $page = 1, 
+        $status = ['live'], 
+        $filters = null
+    ) : \craft\elements\db\TagQuery {
 
-	// Public Methods
-	// =========================================================================
+        $query = new TagQuery(Tag::class);
 
+        $query->with(['group']);
 
+        $this->normalizeTagsQueryFilters($query, $filters);
+        $this->normalizeSortingFilters($query, $filters);
 
-	/**
-	 * globals
-	 *
-	 * @param int $limit 
-	 * @param int $page 
-	 * @param array $status 
-	 * @param mixed $filters 
-	 *
-	 * @return \craft\elements\db\GlobalSetQuery
-	 */
-	public function globals(
-		int $limit = 20, 
-		int $page = 1, 
-		array $status = ['live'], 
-		mixed $filters = null
-	) : \craft\elements\db\GlobalSetQuery {
+        return $query;
 
-		$query = new GlobalSetQuery(GlobalSet::class);
+    }
 
-		$this->normalizeTagsQueryFilters($query, $filters);
-		$this->normalizeSortingFilters($query, $filters);
+    /**
+     * categories
+     *
+     * @param int $limit 
+     * @param int $page 
+     * @param array $status 
+     * @param mixed $filters 
+     *
+     * @return \craft\elements\db\TagQuery
+     */
+    public function categories(
+        int $limit = 20, 
+        int $page = 1, 
+        array $status = ['live'], 
+        mixed $filters = null
+    ) : \craft\elements\db\TagQuery {
 
-		return $query;
+        // if (is_string($group)) $group = Craft::$app->tags->getTagGroupByHandle($group)->id;
 
-	}
+        $query = new CategoryQuery(Category::class);
 
+        $query->with(['group']);
 
+        $this->normalizeCategoriesQueryFilters($query, $filters);
+        $this->normalizeSortingFilters($query, $filters);
 
-	/**
-	 * tags
-	 *
-	 * @param int $limit 
-	 * @param int $page 
-	 * @param array $status 
-	 * @param mixed $filters 
-	 *
-	 * @return \craft\elements\db\TagQuery
-	 */
-	public function tags(
-		$limit = 20, 
-		$page = 1, 
-		$status = ['live'], 
-		$filters = null
-	) : \craft\elements\db\TagQuery {
+        return $query;
 
-		$query = new TagQuery(Tag::class);
+    }
 
-		$query->with(['group']);
+    public function csrf() {
 
-		$this->normalizeTagsQueryFilters($query, $filters);
-		$this->normalizeSortingFilters($query, $filters);
+        return $this->getCsrfBasics();
 
-		return $query;
+    }
 
-	}
+    /**
+     * resolveFilters
+     *
+     * @param \craft\web\Request $request 
+     *
+     * @return array
+     */
+    public function resolveFilters(
+        \craft\web\Request $request
+    ) : array {
 
+        $ignore = ['limit', 'page'];
 
+        $settings = $request->resolve()[1];
+        $filters = array_filter(
+            $settings, 
+            function ($k) use ($ignore) { return !in_array($k, $ignore); },
+            ARRAY_FILTER_USE_KEY
+        );
 
-	/**
-	 * categories
-	 *
-	 * @param int $limit 
-	 * @param int $page 
-	 * @param array $status 
-	 * @param mixed $filters 
-	 *
-	 * @return \craft\elements\db\TagQuery
-	 */
-	public function categories(
-		int $limit = 20, 
-		int $page = 1, 
-		array $status = ['live'], 
-		mixed $filters = null
-	) : \craft\elements\db\TagQuery {
+        return [
+            array_key_exists('limit', $settings)
+                ? $settings['limit'] : 20,
+            array_key_exists('page', $settings)
+                ? $settings['page'] : 1,
+            ['live'],
+            $filters
+        ];
 
-		// if (is_string($group)) $group = Craft::$app->tags->getTagGroupByHandle($group)->id;
+    }
 
-		$query = new CategoryQuery(Category::class);
 
-		$query->with(['group']);
 
-		$this->normalizeCategoriesQueryFilters($query, $filters);
-		$this->normalizeSortingFilters($query, $filters);
 
-		return $query;
+    
+    // Protected Methods
+    // =========================================================================
 
-	}
+    protected function normalizeTagsQueryFilters(
+        $query, 
+        $filters
+    ) {
 
+        foreach($filters as $name => $value) {
 
+            switch ($name) {
 
-	public function csrf() {
+                case 'group':
 
-		return $this->getCsrfBasics();
+                    $query->group($value);
+                    break;
 
-	}
+            }
 
+        }
 
+    }
 
-	/**
-	 * resolveFilters
-	 *
-	 * @param \craft\web\Request $request 
-	 *
-	 * @return array
-	 */
-	public function resolveFilters(
-		\craft\web\Request $request
-	) : array {
+    protected function normalizeCategoriesQueryFilters(
+        $query, 
+        $filters
+    ) {
 
-		$ignore = ['limit', 'page'];
+        foreach($filters as $name => $value) {
 
-		$settings = $request->resolve()[1];
-		$filters = array_filter(
-			$settings, 
-			function ($k) use ($ignore) { return !in_array($k, $ignore); },
-			ARRAY_FILTER_USE_KEY
-		);
+            switch ($name) {
 
-		return [
-			array_key_exists('limit', $settings)
-				? $settings['limit'] : 20,
-			array_key_exists('page', $settings)
-				? $settings['page'] : 1,
-			['live'],
-			$filters
-		];
+                case 'group':
 
-	}
+                    $query->group($value);
+                    break;
 
+            }
 
+        }
 
+    }
 
-	
-	// Protected Methods
-	// =========================================================================
+    protected function normalizeSortingFilters(
+        $query, 
+        $filters
+    ) {
 
-	protected function normalizeTagsQueryFilters(
-		$query, 
-		$filters
-	) {
+        $criteria = $this->normalizeSortingCriteria(
+            (array_key_exists('order_by', $filters)) ? explode(';', $filters['order_by']) : []
+        );
 
-		foreach($filters as $name => $value) {
+        foreach($criteria as $name => $direction) {
 
-			switch ($name) {
+            switch ($name) {
 
-				case 'group':
 
-					$query->group($value);
-					break;
+            }
 
-			}
+        }
 
-		}
+        if (!count($criteria)) {
 
-	}
+            $query->addOrderBy(
+                'title ASC'
+            );
 
-	protected function normalizeCategoriesQueryFilters(
-		$query, 
-		$filters
-	) {
+        }
 
-		foreach($filters as $name => $value) {
+        return $query;
+        
+    }
 
-			switch ($name) {
+    protected function resolveJSONGet(
+        $request, 
+        $id
+    ) {
 
-				case 'group':
+        $result = null;
 
-					$query->group($value);
-					break;
+        switch ($id) {
 
-			}
+            case 'categories':
 
-		}
+                $result = $this->resolveCategoriesJSONGet($request);
+                break;
 
-	}
+            case 'tags':
 
-	protected function normalizeSortingFilters(
-		$query, 
-		$filters
-	) {
+                $result = $this->resolveTagsJSONGet($request);
+                break;
 
-		$criteria = $this->normalizeSortingCriteria(
-			(array_key_exists('order_by', $filters)) ? explode(';', $filters['order_by']) : []
-		);
+            case 'csrf':
 
-		foreach($criteria as $name => $direction) {
+                $result = $this->resolveCSRFGet($request);
+                break;
 
-			switch ($name) {
+        }
 
+        return $result;
 
-			}
+    }
 
-		}
+    protected function resolveCSRFGet(
+        $request
+    ) {
 
-		if (!count($criteria)) {
+        return $this->csrf();
 
-			$query->addOrderBy(
-				'title ASC'
-			);
+    }
 
-		}
+    protected function resolveCategoriesJSONGet(
+        $request, 
+        $entries = null
+    ) {
 
-		return $query;
-		
-	}
+        $entries = $entries ?? $this->categories(...$this->resolveFilters($request))->all();
 
-	protected function resolveJSONGet(
-		$request, 
-		$id
-	) {
+        $items = [];
 
-		$result = null;
+        foreach ($entries as $entry) {
 
-		switch ($id) {
+            $item = [
+                "meta" => [
+                    "id" => $entry->id,
+                    "groupId" => $entry->groupId,
+                    "slug" => $entry->slug,
+                ],
+                "title" => $entry->title,
+                "description" => $entry->description,
+                "parent" => null
+            ];
 
-			case 'categories':
+            if ($entry->parent !== null) {
+                
+                $item['parent'] = [
+                    "meta" => [
+                        "id" => $entry->parent->id,
+                        "groupId" => $entry->parent->groupId,
+                        "slug" => $entry->parent->slug
+                    ],
+                    "title" => $entry->title,
+                    "description" => $entry->description
+                ];
 
-				$result = $this->resolveCategoriesJSONGet($request);
-				break;
+            }
 
-			case 'tags':
+            $items[$entry->slug] = $item;
 
-				$result = $this->resolveTagsJSONGet($request);
-				break;
+        }
 
-			case 'csrf':
+        return $items;
 
-				$result = $this->resolveCSRFGet($request);
-				break;
+    }
 
-		}
+    protected function resolveTagsJSONGet(
+        $request, 
+        $entries = null
+    ) {
 
-		return $result;
+        $entries = $entries ?? $this->tags(...$this->resolveFilters($request))->all();
 
-	}
+        $items = [];
 
-	protected function resolveCSRFGet(
-		$request
-	) {
+        foreach ($entries as $entry) {
 
-		return $this->csrf();
+            $item = [
+                "meta" => [
+                    "id" => $entry->id,
+                    "groupId" => $entry->groupId,
+                    "slug" => $entry->slug,
+                ],
+                "title" => $entry->title,
+                "description" => $entry->description
+            ];
 
-	}
+            $items[$entry->slug] = $item;
 
-	protected function resolveCategoriesJSONGet(
-		$request, 
-		$entries = null
-	) {
+        }
 
-		$entries = $entries ?? $this->categories(...$this->resolveFilters($request))->all();
+        return $items;
 
-		$items = [];
-
-		foreach ($entries as $entry) {
-
-			$item = [
-				"meta" => [
-					"id" => $entry->id,
-					"groupId" => $entry->groupId,
-					"slug" => $entry->slug,
-				],
-				"title" => $entry->title,
-				"description" => $entry->description,
-				"parent" => null
-			];
-
-			if ($entry->parent !== null) {
-				
-				$item['parent'] = [
-					"meta" => [
-						"id" => $entry->parent->id,
-						"groupId" => $entry->parent->groupId,
-						"slug" => $entry->parent->slug
-					],
-					"title" => $entry->title,
-					"description" => $entry->description
-				];
-
-			}
-
-			$items[$entry->slug] = $item;
-
-		}
-
-		return $items;
-
-	}
-
-	protected function resolveTagsJSONGet(
-		$request, 
-		$entries = null
-	) {
-
-		$entries = $entries ?? $this->tags(...$this->resolveFilters($request))->all();
-
-		$items = [];
-
-		foreach ($entries as $entry) {
-
-			$item = [
-				"meta" => [
-					"id" => $entry->id,
-					"groupId" => $entry->groupId,
-					"slug" => $entry->slug,
-				],
-				"title" => $entry->title,
-				"description" => $entry->description
-			];
-
-			$items[$entry->slug] = $item;
-
-		}
-
-		return $items;
-
-	}
+    }
 
 }
